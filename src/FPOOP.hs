d924b08e547a00ef1e59286a7c3bf76ae94289c7
Sergey Mironov <grrwlf@gmail.com>
2018-07-21 22:55:44 +0300

    Prettier names, new concept (draft)

--- src/FPOOP.hs
+++ src/FPOOP.hs
@@ -1,17 +1,25 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module FPOOP where
 
 import Data.Map as Map
 
 {-
-    Public and private fields and methods
+ ____       _            _         _____ _      _     _
+|  _ \ _ __(_)_   ____ _| |_ ___  |  ___(_) ___| | __| |___
+| |_) | '__| \ \ / / _` | __/ _ \ | |_  | |/ _ \ |/ _` / __|
+|  __/| |  | |\ V / (_| | ||  __/ |  _| | |  __/ | (_| \__ \
+|_|   |_|  |_| \_/ \__,_|\__\___| |_|   |_|\___|_|\__,_|___/
+
 
     class Foo {
       pribate:
-        int zoo;
-        void foo(int x) { zoo = x; } ;
+        int _x;
+        void g(int x) { _x = x; } ;
       public:
-        Foo() { foo(33); }
-        void bar(int x) { foo(x); return this->zoo; }
+        Foo() { g(33); }
+        void f(int x) { g(x); return this->_x; }
     };
 
 -}
@@ -21,21 +29,92 @@ type Offset = Int
 type Memory = Map (Object,Offset) Int
 
 data Foo = Foo {
-    this :: Object
-  , bar :: Memory -> Foo -> Int -> (Memory,Int)
+    foo_this :: Object
+  , foo_f :: Memory -> Object -> Int -> (Memory,Int)
   }
 
 
 mkFoo :: Memory -> (Memory,Foo)
-mkFoo mem =
+mkFoo mem0 =
   let
-    zoo_offset = 0
+    x_offset = 0
 
-    foo :: Memory -> Object -> Int -> Memory
-    foo m o x = Map.insert (o, zoo_offset) x m
+    g :: Memory -> Object -> Int -> Memory
+    g mem this x = Map.insert (this,x_offset) x mem
 
     new :: Object
     new = 0 -- FIXME: (max mem) + 1
+  in
+  ( g mem0 new 33
+  , Foo new $ \mem this x ->
+       let
+         mem' = g mem this x
+       in
+       (mem', mem' Map.! (this, x_offset) )
+  )
+
+
+{-
+__     ___      _               _                  _   _               _
+\ \   / (_)_ __| |_ _   _  __ _| |  _ __ ___   ___| |_| |__   ___   __| |___
+ \ \ / /| | '__| __| | | |/ _` | | | '_ ` _ \ / _ \ __| '_ \ / _ \ / _` / __|
+  \ V / | | |  | |_| |_| | (_| | | | | | | | |  __/ |_| | | | (_) | (_| \__ \
+   \_/  |_|_|   \__|\__,_|\__,_|_| |_| |_| |_|\___|\__|_| |_|\___/ \__,_|___/
+
+    class Bar {
+      public:
+        virtual bar(int x) = 0;
+    };
+
+    class Baz : public Bar {
+        int _x;
+      public:
+        virtual bar(int x) { _x = x; }
+    };
+
+    class Bax : public Bar {
+        int _y;
+      public:
+        virtual bar(int x) { _y = x; }
+    };
+
+-}
+
+{-
+class Inherits a b where
+  parent :: a -> b
+
+
+data Bar = Bar {
+    bar_this :: Object
+    bar_child :: Object
+  , bar_f :: Memory -> Bar -> Int -> (Memory,Int)
+  }
+
+data Baz = Baz {
+    baz_this :: Object
+  , baz_bar :: Bar
+  , baz_f :: Memory -> Baz -> Int -> (Memory,Int)
+  }
+
+data Bax = Bax {
+    bax_this :: Object
+  , bax_bar :: Bar
+  , bax_f :: Memory -> Baz -> Int -> (Memory,Int)
+  }
+
+instance Inherits Baz Bar where
+  parent Baz{..} = baz_bar
+
+mkBaz :: Memory -> (Memory,Baz)
+mkBaz mem =
+  let
+    new :: Object
+    new = 1 -- FIXME: (max mem) + 1
+
+
+    baz_f :: Memory -> Baz -> Int -> Memory
+    baz_f m baz
   in
   ( foo mem new 33
   , Foo new $ \m o zoo' ->
@@ -44,5 +123,4 @@ mkFoo mem =
        in
        (mem', mem' Map.! (this o, zoo_offset) )
   )
-
-
+-}
